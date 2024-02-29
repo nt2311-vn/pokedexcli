0a9fd20e95d3cc859498f1920075bcb2678625e2
ntEOS <nguyentoan231196@gmail.com>
2024-03-01 00:41:13 +0700

    Add cache struct for entry, and two method add and get to update and write cache

---
+++ pokecache/pokecache.go
@@ -0,0 +1,28 @@
+package pokecache
+
+import "time"
+
+type Cache struct {
+	cache map[string]cacheEntry
+}
+
+type cacheEntry struct {
+	val       []byte
+	createdAt time.Time
+}
+
+func NewCache() Cache {
+	return Cache{
+		cache: make(map[string]cacheEntry),
+	}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.cache[key] = cacheEntry{val: val, createdAt: time.Now().UTC()}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	cacheE, ok := c.cache[key]
+
+	return cacheE.val, ok
+}
