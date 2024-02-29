91c6ff996bd90a270b5c3996dd57bd7153ca25b9
ntEOS <nguyentoan231196@gmail.com>
2024-02-29 23:11:51 +0700

    Implement call request to get maps done

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -1,6 +1,11 @@
 package pokeapi
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func (c *Client) ListLocationAreas() (LocationAreasResp, error) {
 	endpoint := "/location-area"
@@ -10,4 +15,29 @@ func (c *Client) ListLocationAreas() (LocationAreasResp, error) {
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode > 299 {
+		return LocationAreasResp{}, fmt.Errorf("Bad status code: %d", res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(data, &locationAreasResp)
+
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	return locationAreasResp, nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/nt2311-vn/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	// startRepl()
+
+	pokeClient := pokeapi.NewClient()
+
+	res, err := pokeClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
 }
