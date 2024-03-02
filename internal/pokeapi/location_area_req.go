0df557c017c0e5c2fb330edd62e53bddd3a7f7c1
ntEOS <nguyentoan231196@gmail.com>
2024-03-02 13:55:18 +0700

    Add new struct location area and get location area methods on pokeapi

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -49,11 +49,61 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 		return LocationAreasResp{}, err
 	}
 
-	locationAreasResp := LocationAreasResp{}
-	err = json.Unmarshal(data, &locationAreasResp)
+	locationArea := LocationAreasResp{}
+	err = json.Unmarshal(data, &locationArea)
 
 	if err != nil {
 		return LocationAreasResp{}, err
+	}
+
+	c.cache.Add(fullURL, data)
+
+	return locationArea, nil
+}
+
+func (c *Client) GetLocationArea(locationAreaName string) (LocationArea, error) {
+	endpoint := "/location-area/" + locationAreaName
+	fullURL := baseURL + endpoint
+
+	data, ok := c.cache.Get(fullURL)
+
+	if ok {
+		locationArea := LocationArea{}
+		err := json.Unmarshal(data, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+		return locationArea, nil
+
+	}
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode > 299 {
+		return LocationArea{}, fmt.Errorf("Bad status code: %d", res.StatusCode)
+	}
+
+	data, err = io.ReadAll(res.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	locationAreasResp := LocationArea{}
+	err = json.Unmarshal(data, &locationAreasResp)
+
+	if err != nil {
+		return LocationArea{}, err
 	}
 
 	c.cache.Add(fullURL, data)
