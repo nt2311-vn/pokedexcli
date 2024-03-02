d3d96063f5eb2ebc7f516224d8b6f7184f3268da
ntEOS <nguyentoan231196@gmail.com>
2024-03-02 10:51:27 +0700

    Test case that data should be reaped and should be deleted

--- pokecache/pokecache_test.go
+++ pokecache/pokecache_test.go
@@ -67,3 +67,20 @@ func TestReap(t *testing.T) {
 		t.Errorf("%s should have been reaped", keyOne)
 	}
 }
+
+func TestReapFail(t *testing.T) {
+	interval := time.Millisecond * 10
+
+	cache := NewCache(interval)
+
+	keyOne := "key1"
+	cache.Add(keyOne, []byte("val1"))
+
+	time.Sleep(interval / 2)
+
+	_, ok := cache.Get(keyOne)
+
+	if !ok {
+		t.Errorf("%s should not have been reaped", keyOne)
+	}
+}
