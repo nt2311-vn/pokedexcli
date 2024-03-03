aa292c1f45dba6ca4198523d0668169995808c40
ntEOS <nguyentoan231196@gmail.com>
2024-03-03 22:04:08 +0700

    write logic for command_catch

---
+++ command_catch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func callbackCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no pokemon name provided")
+	}
+
+	pokemonName := args[0]
+
+	pokemon, err := cfg.pokeapiClient.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	const threshold = 50
+	randNum := rand.Intn(pokemon.BaseExperience)
+
+	if randNum > threshold {
+		return fmt.Errorf("Failed to catch %s !\n", pokemonName)
+	}
+
+	fmt.Printf("%s was caught!\n", pokemonName)
+	return nil
+}
